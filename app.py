39e43b91a1e661bd7fc53573a7305cce30e7daef
manytaa <talebimani2000@gmail.com>
2025-08-01 22:58:11 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("hi")
+khode_bezan = st.button("بزن رو من")
+khode_nazan = st.button("نزن رو من")
+if khode_bezan == True :
+    st.write("جون")
+else :
+    st.write("مرض داری؟")
